c0f283e6475d299f06d5bbc193b1dde89e104711
unknown <b03901166@ntu.edu.tw>
2018-07-03 15:19:11 +0800

    ready for single play

--- src/tetris-server.js
+++ src/tetris-server.js
@@ -6,49 +6,35 @@ const socketIO = require('socket.io')
 const app = express()
 const server = http.createServer(app)
 const io = socketIO(server)
-var bodyParser = require('body-parser');
-var mysql = require('mysql2');
+var ROWS = 16;
+var COLS = 16;
+var board = [];
+var players = [];
+var playersNUM = 0;
+var shapes = [
+  [1, 1, 1, 1],
+  [1, 1, 1, 0,
+    1],
+  [1, 1, 1, 0,
+    0, 0, 1],
+  [1, 1, 0, 0,
+    1, 1],
+  [1, 1, 0, 0,
+    0, 1, 1],
+  [0, 1, 1, 0,
+    1, 1],
+  [0, 1, 0, 0,
+    1, 1, 1]
+];
+var colors = [
+  'cyan', 'orange', 'blue', 'yellow', 'red', 'green', 'purple'
+];
 
-var config  = {
-  host: "webprog-mysql.mysql.database.azure.com",
-  user: "yzhsieh@webprog-mysql",
-  password: "Smalldog1205",
-  database : 'tetris', 
-  port: 3306,
-  ssl: true
-  
-}
 
-const conn = new mysql.createConnection(config);
-
-
-// conn.connect(
-//   function (err) { 
-//   if (err) { 
-//       console.log("!!! Cannot connect !!! Error:");
-//       throw err;
-//   }
-//   });
-//   conn.query('CREATE TABLE users (name VARCHAR(255), birth INTEGER, wincount INTEGER, losecount INTEGER, score INTEGER);', 
-//   function (err, results, fields) {
-//       if (err) throw err;
-//   console.log('Created users table.');
-// })
-// variables
-// [id, roomName, user1, user2 (if exist)]
-var currentRoom = []
-// var currentRoom = [
-//   [0, "房名一號", "Jack"],
-//   [1, "這也是房名", "John"],
-//   [2, "這還是房名", "Dick"],
-//   [3, "這又是房名", "Kevin"],
-// ]
-var currentUser = []
-var username;
 ///// room id generator
 function* idMaker() {
   var index = 0;
-  while(true)
+  while (true)
     yield index++;
 }
 var gen = idMaker();
@@ -57,69 +43,205 @@ function genRoomId() {
 }
 /////
 
-getRoomById = (id) => {
-  // TEST 
-  for(let i = 0; i<currentRoom.length; i++){
-    if(currentRoom[i][0] == id)
-      return currentRoom[i]
+function init() {
+  for (var y = 0; y < ROWS; ++y) {
+    board[y] = [];
+    for (var x = 0; x < COLS; ++x) {
+      board[y][x] = 0;
+    }
   }
 }
 
+function newShape(p) {
+  var id = Math.floor(Math.random() * shapes.length);
+  var shape = shapes[id]; // maintain id for color filling
+
+  p.current = [];
+  for (var y = 0; y < 4; ++y) {
+    p.current[y] = [];
+    for (var x = 0; x < 4; ++x) {
+      var i = 4 * y + x;
+      if (typeof shape[i] != 'undefined' && shape[i]) {
+        p.current[y][x] = id + 1;
+      }
+      else {
+        p.current[y][x] = 0;
+      }
+    }
+  }
+
+  // new shape starts to move
+  p.freezed = false;
+  // position where the shape will evolve
+  p.currentX = 5;
+  p.currentY = 0;
+}
+
+function valid(p, offsetX, offsetY, newCurrent) {
+  console.log('in valid');
+  // console.log(p);
+  // console.log(p);
+
+
+  offsetX = offsetX || 0;
+  offsetY = offsetY || 0;
+  offsetX = p.currentX + offsetX;
+  offsetY = p.currentY + offsetY;
+  newCurrent = newCurrent || p.current;
+
+  for (var y = 0; y < 4; ++y) {
+    for (var x = 0; x < 4; ++x) {
+      if (newCurrent[y][x]) {
+        if (typeof board[y + offsetY] == 'undefined'
+          || typeof board[y + offsetY][x + offsetX] == 'undefined'
+          || board[y + offsetY][x + offsetX]
+          || x + offsetX < 0
+          || y + offsetY >= ROWS
+          || x + offsetX >= COLS) {
+          if (offsetY == 1 && p.freezed) {
+            p.lose = true; // lose if the current shape is settled at the top most row
+            document.getElementById('playbutton').disabled = false;
+          }
+          return false;
+        }
+      }
+    }
+  }
+  return true;
+}
+
+function clearLines(p) {
+  for (var y = ROWS - 1; y >= 0; --y) {
+    var rowFilled = true;
+    for (var x = 0; x < COLS; ++x) {
+      if (board[y][x] == 0) {
+        rowFilled = false;
+        break;
+      }
+    }
+    if (rowFilled) {
+      for (var yy = y; yy > 0; --yy) {
+        for (var x = 0; x < COLS; ++x) {
+          board[yy][x] = board[yy - 1][x];
+        }
+      }
+      ++y;
+      p.score += 100
+
+    }
+  }
+}
+
+function rotate(p) {
+  var newCurrent = [];
+  for (var y = 0; y < 4; ++y) {
+    newCurrent[y] = [];
+    for (var x = 0; x < 4; ++x) {
+      newCurrent[y][x] = p.current[3 - x][y];
+    }
+  }
+
+  return newCurrent;
+}
+
+
+
 io.on('connection', socket => {
-  socket.on("login", (msg, fn) =>{
-    username = msg[0]
-    name = msg[0];
-    birth = msg[1];
-    console.log(name, birth)
-    //// make a SQL query
-      var sql = mysql.format("SELECT * FROM `users` WHERE `name` = ? and `birth` = ?", [name, birth])
-      conn.query(sql, (err, res) => {
-        console.log("in Login query")
-        console.log(err);
-        console.log(res);
-        
-        
-        
-        if( res.length !== 0){
-          currentUser.push(res[0].name)
-          rnt = {userData:res[0], lobbyData: currentRoom}
-          console.log(rnt);
-          io.emit("update sideWin", currentUser)
-          fn(rnt)
-          console.log(currentUser);
+
+  console.log('someone connected');
+
+  function tick() {
+    console.log('in tick');
+
+    for (var p = 0; p < playersNUM; p++) {
+      if (valid(players[p], 0, 1)) {
+        ++players[p].currentY;
+      }
+      // if the element settled
+      else {
+        freeze(players[p]);
+        valid(players[p], 0, 1);
+        clearLines(players[p]);
+        if (players[p].lose) {
+          // TODO
+          return false;
         }
-        else{
-          fn(0)
+        // newShape();
+      }
+      io.emit("update", [players, board])
+      // console.log(board);
+      console.log(players[0].current);
+
+
+    }
+  }
+
+  function freeze(p) {
+    console.log('>> in freeze');
+
+    for (var y = 0; y < 4; ++y) {
+      for (var x = 0; x < 4; ++x) {
+        if (p.current[y][x]) {
+          board[y + p.currentY][x + p.currentX] = p.current[y][x];
         }
-      })
+      }
+    }
+    p.freezed = true;
+    p.score += 10;
+    newShape(p)
+  }
+
+  socket.on("start", (data) => {
+    console.log('game start!');
+    console.log(data);
+
+    players.push(data)
+    playersNUM = players.length
+    init()
+    newShape(players[0])
+    setInterval(tick, 400)
   })
 
-  socket.on("register", (msg, fn) =>{
-    name = msg[0];
-    birth = msg[1];
-    //// make a SQL query
-      var sql = mysql.format("SELECT * FROM `users` WHERE `name` = ? and `birth` = ?", [name, birth])
-      conn.query(sql, (err, res) => {
-        console.log("in Register query")
-        console.log(res);
-        if( res === undefined || res.length === 0 ){
-          console.log("new profile, insert to sql")
-          post = {name:name, birth:birth, wincount:0, losecount:0, score:0}
-          conn.query("INSERT INTO users SET ?", post, (err, result, fields) => {
-            if(err) throw err;
-            // console.log('inserted');
-            // console.log(result);
-            // console.log(fields);
-            // fn(result)            
-          })
-        }
-        else{
-          console.log('The account is already exist');
-          fn(0)
-        }
-      })
-  })
 
+  socket.on("move", (arr, fn) => {
+    console.log('move');
+
+    let p = players[arr[0]]
+    let key = arr[1]
+    console.log(key);
+
+    switch (key) {
+      case 'left':
+        if (valid(p, -1)) {
+          --p.currentX;
+        }
+        break;
+      case 'right':
+        if (valid(p, 1)) {
+          ++p.currentX;
+        }
+        break;
+      case 'down':
+        if (valid(p, 0, 1)) {
+          ++p.currentY;
+        }
+        break;
+      case 'rotate':
+        var rotated = rotate(p);
+        if (valid(p, 0, 0, rotated)) {
+          p.current = rotated;
+        }
+        break;
+      // TODO : drop may have some bug
+      case 'drop':
+        while (valid(p, 0, 1)) {
+          ++p.currentY;
+        }
+        tick();
+        break;
+    }
+    io.emit("update", [players, board])
+  })
   socket.on("get room list", (fn) => {
     fn(currentRoom)
   })
@@ -146,18 +268,18 @@ io.on('connection', socket => {
     console.log('in enter room function');
     console.log('user:', user, "roomid:", roomId);
     console.log(roomPtr);
-    if(roomPtr !== undefined){
+    if (roomPtr !== undefined) {
       // check if room is full
-      if(roomPtr.length == 4)
+      if (roomPtr.length == 4)
         fn(0)
-      else{
+      else {
         console.log('entered');
         roomPtr.push(user)
         fn(roomPtr)
         io.emit("update lobby", currentRoom)
         io.to(roomId).emit("update room", roomPtr)
-      console.log(io.sockets.adapter.rooms);
-        
+        console.log(io.sockets.adapter.rooms);
+
       }
     }
     console.log(roomPtr);
@@ -168,35 +290,30 @@ io.on('connection', socket => {
     let roomId = arr[1];
     socket.leave(roomId)
     let roomPtr = getRoomById(roomId);
-    if(roomPtr != undefined){
+    if (roomPtr != undefined) {
       roomPtr.splice(roomPtr.indexOf(userId), 1)
       console.log('in leave room function');
-      if(roomPtr.length == 2){
+      if (roomPtr.length == 2) {
         console.log('there is no person in room, remove it');
         currentRoom.splice(currentRoom.indexOf(roomPtr), 1)
       }
       console.log(currentRoom);
       io.to(roomId).emit("update room", roomPtr)
       console.log(io.sockets.adapter.rooms);
-      
+
       io.emit("update lobby", currentRoom)
       fn(1)
     }
   })
 
 
-  socket.on("get sideWinData", (fn) => {
-    fn(currentUser)
-  })
-
-
-  socket.on('disconnect', () => {
-    console.log(username, 'disconnected')
-    currentUser.splice(currentUser.indexOf(username), 1)
-  })
+  // socket.on('disconnect', () => {
+  //   console.log(username, 'disconnected')
+  //   currentUser.splice(currentUser.indexOf(username), 1)
+  // })
 })
 
 server.listen(3010, function () {
-    console.log('Chat server listen on port 3010!');
-  });
+  console.log('Chat server listen on port 3010!');
+});
 
