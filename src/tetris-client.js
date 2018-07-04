ff300b118cef62871bc3f0b24265cf8d8e0a099a
unknown <b03901166@ntu.edu.tw>
2018-07-04 19:21:56 +0800

    change birth input type

--- src/tetris-client.js
+++ src/tetris-client.js
@@ -164,7 +164,7 @@ class Tetris extends React.Component {
 
     render() {
         return <div ref="div">
-        <canvas ref="canvas" width={960} height={600}  />
+        <canvas ref="canvas" width={640} height={480}  />
         </div>
     }
     // QQ
